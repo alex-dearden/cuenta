d0195d3cc3712bd23023257e4f53c1adf50ecf65
Alex Dearden <alex@kakuweb.com>
2023-07-17 15:06:06 +0100

    Initial Commit

---
+++ Cuenta+/Cuenta_App.swift
@@ -0,0 +1,17 @@
+//
+//  Cuenta_App.swift
+//  Cuenta+
+//
+//  Created by Alex Dearden on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Cuenta_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
