4841152ec60c7466f0842628a618099d33bf5589
Alex Dearden <alex@kakuweb.com>
2023-07-21 15:40:52 +0100

    Made IconImage enum conform to string and Identifiable and CaseIterative for easier use inside a Picker

--- Cuenta+/Models/Video.swift
+++ Cuenta+/Models/Video.swift
@@ -5,28 +5,23 @@ struct Video: Identifiable, Hashable {
     let name: String
     let icon: IconImage
     
-    enum IconImage {
-        case videoImage
-        case regularImage
-        case lizard
-    }
-    
-    ///: I could have the enum conform to String and make these changes above in its initialisation
-    /// but this way gives a more declarative approach and keeps the enum more flexible for future optimisation
-    func iconName() -> String {
-        switch self.icon {
-        case .videoImage:
-            return "video"
-        case .regularImage:
-            return "camera"
-        case .lizard:
-            return "lizard"
+    enum IconImage: String, CaseIterable, Identifiable {
+        case videoImage = "video"
+        case regularImage = "camera"
+        case lizard = "lizard"
+        
+        var name: String {
+            rawValue.lowercased()
+        }
+        
+        var id: String {
+            name
         }
     }
     
     ///: We will probably need the image at some point, outside of a Label element
     func iconImage() -> Image {
-        return Image(systemName: iconName())
+        return Image(systemName: icon.name)
     }
 }
 
--- Cuenta+/Views/MainView.swift
+++ Cuenta+/Views/MainView.swift
@@ -13,23 +13,34 @@ struct MainView: View {
     
     var body: some View {
         LazyVStack(spacing: 20) {
-            Button(action: {
-                videos.append(Video(name: "new one", icon: .lizard))
-            }, label: {
-                Text("Tap to create new Video")
-            }
-            )
+            HStack(spacing: 50) {
+                Button(action: {
+                    videos.append(Video(name: "new one", icon: .lizard))
+                }, label: {
+                    Label("Add video", systemImage: "doc.fill.badge.plus")
+                        .foregroundColor(.purple)
+                })
+                
+                Spacer()
+                
+                Button(action: {
+                    showDetail.toggle()
+                }, label: {
+                    Label("Details", systemImage: "display")
+                        
+                })
+            }.padding(.bottom, 40)
             
             ForEach(videos) { video in
                 HStack {
-                    Label(video.name, systemImage: video.iconName())
+                    Label(video.name, systemImage: video.icon.name)
                 }
             }
             .frame(width: 300)
-            Button("Present sheet") {
-                showDetail.toggle()
-            }
+            
         }
+        .padding()
+        .controlSize(.large)
         .sheet(isPresented: $showDetail) {
             DetailView(isPresented: $showDetail)
         }
@@ -39,6 +50,30 @@ struct MainView: View {
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView(videos: Video.mockVideos)
+    }
+}
+
+struct IconView: View {
+    let icon: Image
+    
+    var body: some View {
+        icon
+    }
+}
+
+struct AddView: View {
+    @State private var name: String = ""
+    @State private var icon: Video.IconImage = .videoImage
+    
+    var body: some View {
+        HStack {
+            TextField("Name", text: $name)
+            Picker("Icon", selection: $icon, content: {
+//                ForEach(Video.IconImage.allCases) { icon in
+//                    IconView(icon: icon)
+//                }
+            })
+        }
     }
 }
 
