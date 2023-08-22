b5fa5ba34f076dfda85bf72f0b3140f4c519ea84
Alex Dearden <alex@kakuweb.com>
2023-08-22 13:35:15 +0100

    Added managers

--- Cuenta+/Managers/VideoDataManager.swift
+++ Cuenta+/Managers/VideoDataManager.swift
@@ -9,12 +9,24 @@ import Foundation
 
 protocol Fetchable {
     /// We don't care how this is imiplemented, just give us an array of Video back
-    func getVideos() -> [Video]
+    func getVideos() async -> [ExternalVideo]
+}
+
+protocol GenericFetchable {
+    associatedtype V
+    func getItems() -> [V]
 }
 
 struct VideoDataManager: Fetchable {
-    func getVideos() -> [Video] {
+    func getVideos() async -> [ExternalVideo] {
+        do {
+            let videos = try await URLSession.shared.decode([ExternalVideo].self, from: Bundle.main.url(forResource: "videos", withExtension: "json")!)
+            return videos
+        } catch {
+            print("Error: \(error)")
+        }
         
+        return []
     }
     
 }
---
+++ Cuenta+/Models/ExternalVideo.swift
@@ -0,0 +1,11 @@
+/// This is simply so we can test and mess around
+/// in a real app we would NEVER have 2 different models!
+
+import Foundation
+
+struct ExternalVideo: Decodable {
+    let id: String
+    let name: String
+    let author: String
+    let icon: String
+}
