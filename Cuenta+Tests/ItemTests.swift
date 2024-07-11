555a63f4ad36a78e26ab544c7b6b7b04b33f6a58
Alex Dearden <alex@kakuweb.com>
2024-07-11 21:47:21 +0100

    Small test optimisation

--- Cuenta+Tests/ItemTests.swift
+++ Cuenta+Tests/ItemTests.swift
@@ -10,35 +10,34 @@ import XCTest
 
 final class ItemTests: XCTestCase {
     private var userManager: UserManager!
+    private var sortedInterests: [InterestModel]!
     
     override func setUpWithError() throws {
         userManager = UserManager(
             user: .init(name: "Mariana", email: "mana@email.com", password: "mana"),
             isLoggedIn: true
         )
-     
+        
+        sortedInterests = Defaults.allInterestsSorted
     }
 
     override func tearDownWithError() throws {
         userManager = nil
+        sortedInterests = nil
     }
     
     func testSortedInterests() {
-        let sortedInterests = Defaults.allInterestsSorted
-        
         XCTAssertEqual(sortedInterests.first?.name, "Communication")
     }
     
     func testAddInterest() {
-        let sortedInterests = Defaults.allInterestsSorted
         let interestToAdd = sortedInterests.first!
         userManager.addInterest(interestToAdd)
         XCTAssertEqual(userManager.interests.count, 1)
         XCTAssertEqual(userManager.interests.first?.name, "Communication")
     }
 
-    func testRemoveInterest() {
-        let sortedInterests = Defaults.allInterestsSorted
+    func testRemoveInterest() {        
         userManager.addInterest(sortedInterests[0])
         userManager.addInterest(sortedInterests[3])
         XCTAssertEqual(userManager.interests.count, 2)
